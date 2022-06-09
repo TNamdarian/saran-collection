f4df33237240103f484a5c931d3e5385caa29b71
TNamdarian <teemanamdarian@gmail.com>
2022-06-09 13:41:47 +0000

    Added meida and static to settings.py

--- teema_real_estate/settings.py
+++ teema_real_estate/settings.py
@@ -150,10 +150,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIR = [
-    os.path.join(BASE_DIR, 'teema_real_estate/static')
-]
+STATICFILES_DIR = (os.path.join(BASE_DIR, 'static'),)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field

83f340743fdde57fb93bfaccab1aecd525eb3d9e
TNamdarian <teemanamdarian@gmail.com>
2022-06-15 10:21:26 +0000

    Change the name of the app

--- saran_collection/urls.py
+++ saran_collection/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('pages.urls')),
-    path('listings/', include('listings.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

f35604d29b4e0f268008efe47bbb472b3560ec98
TNamdarian <teemanamdarian@gmail.com>
2022-06-15 10:38:46 +0000

    Custumised admin

--- products/models.py
+++ products/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 
 class Category(models.Model):
+
+    class Meta:
+        """
+        To ensure the plural of Catergory is correcrly specified.
+        """
+        verbose_name_plural = 'Categories'
+        
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 

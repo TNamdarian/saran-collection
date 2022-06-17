7f47910cae9e5337a236159403353f95eac26fd8
TNamdarian <teemanamdarian@gmail.com>
2022-06-17 14:51:41 +0000

    Added sorting

--- home/apps.py
+++ home/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class PagesConfig(AppConfig):
+class HomeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'pages'
+    name = 'home'
---
+++ home/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    """
+    A view to return the home page
+    """
+    return render(request, 'home/index.html')

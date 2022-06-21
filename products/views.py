1e77236f4c6137381012004002e3a87acc3d7621
TNamdarian <teemanamdarian@gmail.com>
2022-06-21 12:58:10 +0000

    Added content to toast_success

--- products/views.py
+++ products/views.py
@@ -8,7 +8,6 @@ def all_products(request):
     """
     A view to show all products, including sorting and search queries
     """
-
     products = Product.objects.all()
     query = None
     categories = None

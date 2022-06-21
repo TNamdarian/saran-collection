7a4b950e954582ffe2ccfdb74f6d8fa6ca877cc4
TNamdarian <teemanamdarian@gmail.com>
2022-06-21 10:10:49 +0000

    Added toasts

--- bag/views.py
+++ bag/views.py
@@ -35,6 +35,7 @@ def add_to_bag(request, item_id):
             bag[item_id] += quantity
         else:
             bag[item_id] = quantity
+            messages.success(request, f'Added {product.name} to your bag')
 
     request.session['bag'] = bag
     return redirect(redirect_url)

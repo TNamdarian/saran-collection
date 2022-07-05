ff22cdd9821446eb4af1928607b63af09a52fa7e
TNamdarian <teemanamdarian@gmail.com>
2022-07-05 11:51:33 +0000

    Sending real emails

--- saran_collection/settings.py
+++ saran_collection/settings.py
@@ -215,7 +215,7 @@ DEFAULT_FROM_EMAIL = 'contact@sarancollection.co.uk'
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'happybean.info@gmail.com'
+    DEFAULT_FROM_EMAIL = 'contact@sarancollection.co.uk'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True

942d5036fd0a756e28bb59b672366028adfd7a10
TNamdarian <teemanamdarian@gmail.com>
2022-07-10 14:38:12 +0000

    Added products to fixtures

---
+++ BACKUP/products/admin.py
@@ -0,0 +1,33 @@
+from django.contrib import admin
+from .models import Product, Category
+
+
+
+class ProductAdmin(admin.ModelAdmin):
+    """
+    Product admin
+    """
+    list_display = (
+        'category',
+        'sku',
+        'name',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    """
+    Category Admin
+    """
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)

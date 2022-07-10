fbe04684a41cbace0f7567ac4b444c49761eb935
TNamdarian <teemanamdarian@gmail.com>
2022-07-10 12:10:47 +0000

    Added images of suits to images and fixtures

--- products/views.py
+++ products/views.py
@@ -26,8 +26,6 @@ def all_products(request):
                 products = products.annotate(lower_name=Lower('name'))
             if sortkey == 'category':
                 sortkey = 'category__name'
-            if sortkey == 'origin':
-                sortkey = 'origin__name'
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':

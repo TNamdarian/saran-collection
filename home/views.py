b7f9e5e19ba8a89c7464c7d71aa8235e16a8816d
TNamdarian <teemanamdarian@gmail.com>
2022-07-11 14:27:56 +0000

    Added best sellers to index.html views and template

--- home/views.py
+++ home/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from products.models import Product
 
 
 def index(request):
     """
     A view to return the home page
     """
-    return render(request, 'pages/index.html')
+    products = Product.objects.order_by('-rating')[:6]
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'pages/index.html', context)
